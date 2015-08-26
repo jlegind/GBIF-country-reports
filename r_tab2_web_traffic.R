03f765982fecfdea11851fdfe3827d64cccd48eb
jlegind <klegind@gmail.com>
2015-08-26 15:58:25 +0200

    Table 2 web traffic - Worldwide and National

---
+++ r_tab2_web_traffic.R
@@ -0,0 +1,32 @@
+#Creates Table 2; worldwide and national figures for web traffic
+
+query.list <- Init(start.date = "2014-07-01",
+                   end.date = "2015-06-30",                   
+                   dimensions = "ga:countryIsoCode",
+                   metrics = "ga:sessions, ga:pageviewsPerSession, ga:avgSessionDuration, ga:bounceRate, ga:percentNewSessions",                   
+                   max.results = 10000,
+                   #sort = "ga:countryIsoCode, -ga:sessions",
+                   table.id = "ga:73962076")
+
+ga.query <- QueryBuilder(query.list)
+ga.data <- GetReportData(ga.query, token)
+#Pagination required
+
+ga.data[,3:6] <- data.frame(sapply(ga.data[,3:6], FUN=function(x) format(round(as.numeric(x), digits = 2), nsmall = 2)), stringsAsFactors = F)
+#Converts char columns to numeric and enforced two digit format
+
+web_traffic <- data.frame(left_join(iso_country, ga.data, by=c('iso_code'='countryIsoCode')))
+web_traffic[is.na(web_traffic)] <- "no data"
+
+#For the "Worldwide" column which is static across all reports
+query.list <- Init(start.date = "2014-07-01",
+                   end.date = "2015-06-30",                   
+                   #dimensions = "ga:countryIsoCode",
+                   metrics = "ga:sessions, ga:pageviewsPerSession, ga:avgSessionDuration, ga:bounceRate, ga:percentNewSessions",                   
+                   max.results = 10000,
+                   #sort = "ga:countryIsoCode, -ga:sessions",
+                   table.id = "ga:73962076")
+
+ga.query <- QueryBuilder(query.list)
+worldwide <- GetReportData(ga.query, token)
+worldwide[,2:5] <- data.frame(sapply(worldwide[,2:5], FUN=function(x) format(round(as.numeric(x), digits = 2), nsmall = 2)), stringsAsFactors = F)
