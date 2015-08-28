822e3bc2b433b14bdf0e8d5fbfe9a8db026536d4
jlegind <klegind@gmail.com>
2015-08-28 17:34:53 +0200

    replaced r_download_blob.r

---
+++ r_page3_download_blob.R
@@ -0,0 +1,10 @@
+library(dplyr)
+download_blob<-read.csv("downloads_blob.csv", na.strings="NAN")
+iso_country <- read.csv("country_iso_GBIF.csv", encoding="UTF-8", na.strings="NAN") 
+
+download_blob <- left_join(iso_country, download_blob, by=c('iso_code'='iso_code'),type="left")
+
+download_blob[is.na(download_blob)]<-0
+
+
+
