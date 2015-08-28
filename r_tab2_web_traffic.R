87068274b44bc613efcf74cf64481bc22c6f5397
jlegind <klegind@gmail.com>
2015-08-28 13:23:30 +0200

    Redundant code removed

--- r_tab2_web_traffic.R
+++ r_tab2_web_traffic.R
@@ -4,13 +4,11 @@ query.list <- Init(start.date = "2014-07-01",
                    end.date = "2015-06-30",                   
                    dimensions = "ga:countryIsoCode",
                    metrics = "ga:sessions, ga:pageviewsPerSession, ga:avgSessionDuration, ga:bounceRate, ga:percentNewSessions",                   
-                   max.results = 10000,
-                   #sort = "ga:countryIsoCode, -ga:sessions",
+                   max.results = 10000,                   
                    table.id = "ga:73962076")
 
 ga.query <- QueryBuilder(query.list)
 ga.data <- GetReportData(ga.query, token)
-#Pagination required
 
 ga.data[,3:6] <- data.frame(sapply(ga.data[,3:6], FUN=function(x) format(round(as.numeric(x), digits = 2), nsmall = 2)), stringsAsFactors = F)
 #Converts char columns to numeric and enforced two digit format
@@ -20,11 +18,9 @@ web_traffic[is.na(web_traffic)] <- "no data"
 
 #For the "Worldwide" column which is static across all reports
 query.list <- Init(start.date = "2014-07-01",
-                   end.date = "2015-06-30",                   
-                   #dimensions = "ga:countryIsoCode",
+                   end.date = "2015-06-30",                                      
                    metrics = "ga:sessions, ga:pageviewsPerSession, ga:avgSessionDuration, ga:bounceRate, ga:percentNewSessions",                   
-                   max.results = 10000,
-                   #sort = "ga:countryIsoCode, -ga:sessions",
+                   max.results = 10000,                   
                    table.id = "ga:73962076")
 
 ga.query <- QueryBuilder(query.list)
