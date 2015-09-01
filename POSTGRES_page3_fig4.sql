9c4c1641507283b818e4ff9ec98c232df114d251
jlegind <klegind@gmail.com>
2015-09-01 15:35:39 +0200

    Fig4 feeds the r_page3_fig4_occ_recs_downloaded_by_month.R scripts

---
+++ POSTGRES_page3_fig4.sql
@@ -0,0 +1,6 @@
+﻿SELECT sum(dod.number_records), o.country, to_char(od.created, 'YYYY-MM') year_month FROM organization o
+JOIN dataset d ON d.publishing_organization_key = o.key
+JOIN dataset_occurrence_download dod ON d.key = dod.dataset_key
+JOIN occurrence_download od ON od.key = dod.download_key
+WHERE date(od.created) BETWEEN '2014-07-01' AND '2015-06-30'
+GROUP BY 2,3 ORDER BY 2,3
