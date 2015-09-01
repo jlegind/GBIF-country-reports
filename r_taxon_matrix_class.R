9e8d5333877ed503c184493cee3c8e67801bca5b
jlegind <klegind@gmail.com>
2015-09-01 14:21:51 +0200

    Class matrix - including the new taxon IDs
    The enforcer data frame is actually needed to get equal numer of columns
    for each country:
    Andorra only has 6 classes out of the 7 needed.

--- r_taxon_matrix_class.R
+++ r_taxon_matrix_class.R
@@ -1,21 +1,28 @@
 library(dplyr)
-mycsv<-read.csv("class_matrix.csv", na.strings="NAN")
-enfor<-read.csv("class_matrix_group_enforce.csv", na.strings="NAN")
-combo<-left_join(enfor,mycsv,by=c('t2.class_'='class_matrix.class_', 't1.iso_code'='class_matrix.country'),type="left")
-combo<- data.frame(lapply(combo, as.character), stringsAsFactors = FALSE)
-combo[is.na(combo)]<-0
+mycsv<-read.csv("../class_matrix.csv", na.strings="NAN")
+#enfor<-read.csv("class_matrix_group_enforce.csv", na.strings="NAN")
 
-fct<-as.character(unique(combo[,2]))
-countries<-as.character(unique(combo[,1]))
+enforcer <- data.frame(country=character(), taxon=character(),stringsAsFactors = FALSE)
 
-df_class<-data.frame(countries)
+for (j in iso_country[,1]){
+    for (k in unique(mycsv[,2])){        
+        enforcer[nrow(enforcer)+1, ] <- c(j, k)
+    }
+}
+
+combo <- left_join(enforcer, mycsv, by=c('taxon'='jan.class_matrix.class_', 'country'='jan.class_matrix.country'),type="left")
+combo <- data.frame(lapply(combo, as.character), stringsAsFactors = FALSE)
+combo[is.na(combo)] <- 0
+
+fct <- as.character(unique(combo[,2]))
+df_class <- data.frame(iso_country$country)
 
 for (j in fct){
-    newdf<-combo[combo$t2.class_==j, c('class_matrix._c2','class_matrix._c3')]
+    newdf <- combo[combo$taxon == j, c('jan.class_matrix._c2','jan.class_matrix.increase')]
     for (k in colnames(newdf)){
         #print(k)
         #print(head(newdf[,k]))
-        df_class[,paste(k,j, sep="")]<-newdf[,k]    
+        df_class[,paste(k,j, sep="")] <- newdf[,k]    
     }    
 }
 
