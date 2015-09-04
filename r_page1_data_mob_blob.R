c2440e92915fad9fa43dd6e501a4472b7a5b271f
jlegind <klegind@gmail.com>
2015-09-04 10:45:56 +0200

    minor change

--- r_page1_data_mob_blob.R
+++ r_page1_data_mob_blob.R
@@ -1,7 +1,7 @@
 #Requires csv file from HiveQL Page1_data_mobilization_blob.txt
 
 library(dplyr)
-data_mob_blob<-read.csv("page1_data_mob_blob.csv", na.strings="NAN")
+data_mob_blob<-read.csv("../page1_data_mob_blob.csv", na.strings="NAN")
 iso_country <- read.csv("../country_iso_GBIF.csv", encoding="UTF-8", na.strings="NAN") 
 
 data_mob_blob <- left_join(iso_country, data_mob_blob, by=c('iso_code'='pub_country'),type="left")
