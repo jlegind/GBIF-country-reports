1fabd852c79a9a9121fee262beaac1a57bfdf5c2
jlegind <klegind@gmail.com>
2015-09-08 16:56:32 +0200

    additional filters added

--- POSTGRES_page3_fig4.sql
+++ POSTGRES_page3_fig4.sql
@@ -2,5 +2,6 @@
 JOIN dataset d ON d.publishing_organization_key = o.key
 JOIN dataset_occurrence_download dod ON d.key = dod.dataset_key
 JOIN occurrence_download od ON od.key = dod.download_key
-WHERE date(od.created) BETWEEN '2014-07-01' AND '2015-06-30'
+WHERE od.notification_addresses NOT LIKE '%@gbif.org' AND
+  od.status='SUCCEEDED' AND od.created_by != 'nagios' AND o.country = 'US' AND date(od.created) BETWEEN '2014-07-01' AND '2015-06-30'
 GROUP BY 2,3 ORDER BY 2,3
